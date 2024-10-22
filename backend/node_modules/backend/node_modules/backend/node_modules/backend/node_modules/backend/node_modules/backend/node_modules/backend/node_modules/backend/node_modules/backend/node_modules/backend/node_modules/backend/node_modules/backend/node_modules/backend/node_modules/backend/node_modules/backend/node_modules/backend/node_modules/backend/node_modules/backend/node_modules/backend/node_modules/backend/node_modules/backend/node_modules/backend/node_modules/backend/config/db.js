5c3aa9c64d5847c4ba148d172ee05b230d61ab06
nandu <192p1a04b5nandu@gmail.com>
2024-10-22 12:23:08 +0530

    node nodules changed

---
+++ backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/config/db.js
@@ -0,0 +1,15 @@
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            
+        })
+        console.log("MongoDB connection SUCCESS");
+    } catch (error) {
+        console.error("MongoDB connection FAIL");
+        process.exit(1);
+    }
+}
+module.exports = connectDB;
